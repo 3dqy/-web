d4524a40c750a8f699b1594c8c9663a611c5ea83
_3dqy <outhman371994@gmail.com>
2024-01-23 07:03:23 +0300

    new file:   cs50/electric/electric.css 	new file:   cs50/electric/electric.html 	new file:   cs50/electric/electric.js 	deleted:    cs50/ieee 	new file:   cs50/images/OIP.jpeg 	new file:   cs50/images/R.png 	modified:   cs50/main/main.css 	modified:   cs50/main/main.html 	modified:   cs50/main/main.js

--- cs50/electric/electric.js
+++ cs50/electric/electric.js
@@ -6,21 +6,18 @@ function toggleDarkMode() {
     // Toggle dark mode by adding/removing a dark-mode class to the body
     body.classList.toggle('dark-mode');
 
+    // Toggle between light and dark mode logos
+    lightLogo.style.display = body.classList.contains('dark-mode') ? 'none' : 'inline';
+    darkLogo.style.display = body.classList.contains('dark-mode') ? 'inline' : 'none';
+
     // Save the user's preference to local storage
     const isDarkMode = body.classList.contains('dark-mode');
     localStorage.setItem('darkMode', isDarkMode);
-
-    // Toggle between light and dark mode logos based on the updated dark mode state
-    lightLogo.style.display = isDarkMode ? 'none' : 'inline';
-    darkLogo.style.display = isDarkMode ? 'inline' : 'none';
 }
 
 // Check if the user has a dark mode preference and apply it
-document.addEventListener('DOMContentLoaded', () => {
-    const savedDarkMode = localStorage.getItem('darkMode');
-    if (savedDarkMode === 'true') {
-        toggleDarkMode();
-    } 
-});
-
+const savedDarkMode = localStorage.getItem('darkMode');
+if (savedDarkMode === 'true') {
+    toggleDarkMode();
+}
 
